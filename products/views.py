9ce0dd62530b6ec126d61ba0ef1048a956d90116
Bhargavi84 <bhargavi.parikshit@yahoo.com>
2021-09-06 21:10:56 +0000

    Changes in sorting and product counts in product page

--- products/views.py
+++ products/views.py
@@ -25,7 +25,6 @@ def all_products(request):
                 products = products.annotate(lower_name=Lower('name'))
             if sortkey == 'category':
                 sortkey = 'category__name'
-
             if 'direction' in request.GET:
                 direction = request.GET['direction']
                 if direction == 'desc':
@@ -36,6 +35,7 @@ def all_products(request):
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
             products = products.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
 
 
         if 'q' in request.GET:
@@ -48,7 +48,6 @@ def all_products(request):
             products = products.filter(queries)
 
     current_sorting = f'{sort}_{direction}'
-
 
     context = {
         'products': products,
